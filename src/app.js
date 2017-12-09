d2c964d25f2d1bb4b42a992f5883f337099499a5
hari4698 <harhara4698@gmail.com>
2017-12-09 13:36:56 +0530

    testapp

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ export const Sample = () => {
+   
+    return <h1>Hello Dude</h1>
+}
+
+ReactDOM.render(
+    <Sample />,
+    document.getElementById('app')
+  );
---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Sample} from './app'
+
+const style = {
+  width: 100,
+  border:  'solid blue',
+  display: 'inline-block',
+  padding: 2,
+  margin: 2
+}
+
+
+class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+  
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+  
+    tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+  
+    render() {
+      return (
+        <div  >
+          <div >
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+          </div>
+                
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+  );
